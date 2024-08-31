05fb013ded61b0bae4a08e5c0ab86b8feff52f49
Catherine Joyce Rodriguez <143790929+Catherinejoycerodriguez@users.noreply.github.com>
2024-08-31 09:20:59 +0800

    Create grp2_rodriguez_ex1.py

---
+++ grp2_rodriguez_ex1.py
@@ -0,0 +1,40 @@
+# a. Create a simple print script
+# b. Add comment "My first simple script"
+
+# My first simple script
+print("Hello, I'm Rodriguez, Catherine Joyce, B. BSCS 3F")
+
+# Variables
+# a. Create two variables and assign number and name to it.
+age = 25
+student_name = "Catherine Joyce B.Rodriguez"
+
+# b. Then print both values
+print("Age:", age)
+print("Student Name:", student_name)
+
+# Variable Names
+# a. Create 4 variable names using the first names of your group members.
+Mohanida = "Mohanida"
+Meriel = "Meriel"
+Cecille = "Cecille "  # Changed to use underscores
+Jona = "Jona"
+
+# Print the values of the group member variables
+print("Group Member First Names:")
+print(Mohanida)
+print(Meriel)
+print(Cecille)
+print(Jona)
+
+# Many Values to Multiple Variables
+# a. Assign values to multiple variables in one line
+height, weight, shoe_size = 147, 48, 39
+print("Height:", height)
+print("Weight:", weight)
+print("Shoe Size:", shoe_size)
+
+# One Value to Multiple Variables
+# a. Assign the same value to multiple variables in one line
+status1 = status2 = status3 = "Active"
+print("Account Statuses:", status1, status2, status3)
